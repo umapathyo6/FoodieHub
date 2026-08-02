17bd751da44a0bd94edc91696a16433d91de45be
UMAPATHY K <umapthy82488@gmail.com>
2026-08-02 12:29:24 +0530

    Initial project setup with Spring Boot and React

---
+++ FoodieHub/FoodieHub/src/main/java/com/example/FoodieHub/FoodieHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodieHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieHubApplication.class, args);
+	}
+
+}
---
+++ FoodieHub/FoodieHub/src/test/java/com/example/FoodieHub/FoodieHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FoodieHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
